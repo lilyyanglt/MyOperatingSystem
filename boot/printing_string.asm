c9f1cb6fce51975ddc1e09d91da4723d9235f294
lily <lilyyanglt@gmail.com>
2021-12-07 12:34:50 -0800

    Fixed problem with printing_string asm file

--- boot/printing_string.asm
+++ boot/printing_string.asm
@@ -3,23 +3,26 @@
 print_string:
     pusha
     
-    mov ah, 0x0e
+    mov ah, 0x0e        ; setting the a register first byte to be 0e
+    mov dh, 0x0         ; setting the register first byte to be 00
     
     jmp check_cx
     
     check_cx:
-    mov cx, [bx]
-    cmp cx, 0
-    jne print_char
+    mov dl, [bx]        ; copy the value at address bx into the last 8 bits of dx register
+    cmp dl, 0           ; compare only the dl because each character is only 8 bits (1 byte)
+    jne print_char      ; because we ended our string bytes with a 0 byte to indicate end of string
+                        ; if dl is not a zero byte, then jump to print out the character
+                        ; essentially this loop will end until dl is 0, if we don't get there
+                        ; it will continue to jump to print_char routing
 
     popa
     ret
 
     print_char:
-         ; need to figure out
 
-        mov al, cl 
+        mov al, dl      ; this is part of the required step to print the character
         int 0x10
-        add bx, 1
-        jmp check_cx
+        add bx, 1       ; increment the bx value by 1 so we can move to the next character
+        jmp check_cx    
 
